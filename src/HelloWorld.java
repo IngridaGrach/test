5800cb332a4a20c9cdc9169b1c4c8652e1eaffa9
IngridaGrach <34721964+IngridaGrach@users.noreply.github.com>
2019-03-26 21:35:09 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 public class HelloWorld {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello, Mum and Dad!");
+        JOptionPane.showMessageDialog(null, "Hello, Mum!");
     }
 }
